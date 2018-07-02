8c7e5306a832b9e73e36d5f30e50c49a9489f5b1
annavolkova1 <anna__banana@mail.ru>
2018-07-02 09:29:29 +0300

    New test

--- test.py
+++ test.py
@@ -20,29 +20,38 @@ result = dictionaryFromKeys(list1, list2)
 print(result)
 
 
-def assert_dictionaryFromKeys():
-    assert dictionaryFromKeys([1, 2, 3, 4], [2, 3, 4, 5]) == {1: 2, 2: 3, 3: 4, 4: 5}, ('False')
-    assert dictionaryFromKeys([6, 7, 8, 9, 10], [1, 2, 3, 4]) == {6: 1, 7: 2, 8: 3, 9: 4, 10: None}, ('Wrong!')
+def test():
+    k = ['1', '2', '3', '4']
+    v = ['W', 'o', 'r', 'd']
+    expected == {'1': 'W', '2': 'o', '3': 'r', '4': 'd'}
+
+    assert dictionaryFromKeys(k, v) == expected
 
 
-@pytest.mark.parametrize("a,b,expected", [
-    ([1, 2, 3], ['a', 'b', 'c', 'd'], {1: 'a', 2: 'b', 3: 'c'}),
-    ([1, 2, 3], 3, None),
-    (3, ['a', 'b', 'c'], None),
-    ([], ['a', 'b', 'c'], {})
-    ])
+def test1():
+    k = ['1', '2', '3', '4', '5']
+    v = ['W', 'o', 'r', 'd']
+    expected == {'1': 'W', '2': 'o', '3': 'r', '4': 'd', '5': 'None'}
+
+    assert dictionaryFromKeys(k, v) == expected
 
 
-def test_parametr(a, b, expected):
-    assert dictionaryFromKeys(a, b) == expected
-    assert dictionaryFromKeys([1, 2, 3], 3) == None
-    assert dictionaryFromKeys(3,['a', 'b', 'c']) == None
-    assert dictionaryFromKeys([], ['a', 'b', 'c']) == {}
+@pytest.mark.parametrize('k, v', [
+    (['1'], 'dearear'),
+    ('2', ['friend'])
+])
+def test2(k, v):
+    with pytest.raises(TypeError):
+        dictionaryFromKeys(k, v)
 
 
-@given(strat.lists(), strat.lists())
-def test_dictionaryFromKeys(a, b):
-    assert type(dictionaryFromKeys(a, b)) == dict
-    assert len(dictionaryFromKeys(a, b)) == len(a)
+@given(st.lists(st.text()))
+def test3(extra_values):
+    k = ['1', '2', '3', '4', '5']
+    v = ['H', 'e', 'l', 'l', 'o']
+    expected = {'1': 'H', '2': 'e', '3': 'l', '4': 'l', '5': 'o'}
+
+    assert dictionaryFromKeys(k, v) == expected
+
 
 
